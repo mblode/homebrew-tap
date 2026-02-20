312a89f49deefbbad6ec2a5b59daeb0de841bc9e
Matthew Blode <m@blode.co>
2026-02-20 03:33:02 +0000

    Update commandment to 0.1.7

--- Casks/commandment.rb
+++ Casks/commandment.rb
@@ -1,6 +1,6 @@
 cask "commandment" do
-  version "0.1.6"
-  sha256 "cebc8b46865bd398d6a6adde9020c00dae1327f89ef73785837bb6b157d4b4e4"
+  version "0.1.7"
+  sha256 "13a2defd94a3ada3a85a65e53a2ee8a3edc19aeca7bcb19dabf30ab1f167fd8e"
 
   url "https://github.com/mblode/commandment/releases/download/v#{version}/Commandment-#{version}.dmg"
   name "Commandment"
