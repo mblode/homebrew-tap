e7d509d7e94c6a8805a4b08a98ef168b91bf0bd8
Matthew Blode <m@blode.co>
2026-03-01 23:41:17 +0000

    Update rubber-duck to 0.1.11

--- Casks/rubber-duck.rb
+++ Casks/rubber-duck.rb
@@ -1,6 +1,6 @@
 cask "rubber-duck" do
-  version "0.1.10"
-  sha256 "4f75ddbf801e99d15b07c7c9b2d666c2e047c5b13f8be25ce49bbb61d65d58b0"
+  version "0.1.11"
+  sha256 "d193a5be8ec8ae5ab9203555ad35307e733f96dc3e6b1d2da9aa45ae0e3017aa"
 
   url "https://github.com/mblode/rubber-duck/releases/download/v#{version}/RubberDuck-#{version}.dmg"
   name "RubberDuck"
