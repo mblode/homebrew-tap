e718f23602cee80abac2511d87dbe6a4370d22d6
Matthew Blode <m@blode.co>
2026-03-02 05:43:19 +0000

    Update rubber-duck to 0.1.13

--- Casks/rubber-duck.rb
+++ Casks/rubber-duck.rb
@@ -1,10 +1,10 @@
 cask "rubber-duck" do
-  version "0.1.12"
-  sha256 "7e9c5de9376a8f98c294dcb57ab1d93e1f92293933e9d2d1478ce0c11afcda1c"
+  version "0.1.13"
+  sha256 "f793eac0f96edb1795d473a1223fa2de3a9073b2d2431acba2bb67af4e621d04"
 
   url "https://github.com/mblode/rubber-duck/releases/download/v#{version}/RubberDuck-#{version}.dmg"
   name "Rubber Duck"
-  desc "macOS menu bar dictation app using OpenAI transcription"
+  desc "macOS menu bar voice coding agent"
   homepage "https://github.com/mblode/rubber-duck"
 
   depends_on macos: ">= :sequoia"
@@ -12,8 +12,13 @@ cask "rubber-duck" do
 
   app "Rubber Duck.app"
 
+  binary "#{appdir}/Rubber Duck.app/Contents/MacOS/rubber-duck"
+  binary "#{appdir}/Rubber Duck.app/Contents/MacOS/rubber-duck", target: "rubber-duck-daemon"
+
   zap trash: [
     "~/Library/Preferences/co.blode.rubber-duck.plist",
-    "~/Library/Application Support/co.blode.rubber-duck",
+    "~/Library/Application Support/RubberDuck",
+    "/usr/local/bin/rubber-duck",
+    "/usr/local/bin/rubber-duck-daemon",
   ]
 end
