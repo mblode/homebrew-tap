32044e6a868bdf9a004bf392790aa9e2cf6c8fcc
Matthew Blode <m@blode.co>
2026-05-22 00:49:28 +0000

    Add convene cask 0.1.0

---
+++ Casks/convene.rb
@@ -0,0 +1,21 @@
+cask "convene" do
+  version "0.1.0"
+  sha256 "f95eacea569096ff53da91e09cc3f785900bbd72d37865d6a67968afb566bbd6"
+
+  url "https://github.com/mblode/convene/releases/download/v#{version}/Convene-#{version}.dmg"
+  name "Convene"
+  desc "macOS meeting transcription app - BYO OpenAI API key"
+  homepage "https://github.com/mblode/convene"
+
+  depends_on macos: ">= :sequoia"
+  auto_updates true
+
+  app "Convene.app"
+
+  zap trash: [
+    "~/Library/Containers/co.blode.convene",
+    "~/Library/Preferences/co.blode.convene.plist",
+    "~/Library/Application Support/co.blode.convene",
+    "~/Library/Logs/Convene.log",
+  ]
+end
