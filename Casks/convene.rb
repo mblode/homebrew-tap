6bb0e1f40ec2480aaa656a2d4ab4cd0d0b4bf0a4
Matthew Blode <m@blode.co>
2026-07-22 07:05:40 +0000

    Update convene to 0.1.9

--- Casks/convene.rb
+++ Casks/convene.rb
@@ -1,6 +1,6 @@
 cask "convene" do
-  version "0.1.8"
-  sha256 "06923893c4b08d7cb35e2e8956e6df0230827dd64b1a3dc0a8815e88494fba29"
+  version "0.1.9"
+  sha256 "341123596c4bf04bf1e4960ac25c6e5bf0c9964278fd69aa4de6963f8073f4f6"
 
   url "https://github.com/mblode/convene/releases/download/v#{version}/Convene-#{version}.dmg"
   name "Convene"
